b745e4f200676849c7c3e2c5b11a734bc0721bd3
Lee, Younha <younha0088@gmail.com>
2024-02-08 01:45:34 +0900

    feat: Service로 모든 로직 옮기기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,42 +1,6 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts : PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_offical',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지.',
-    likeCount: 100,
-    commentCount: 150,
-  },
-  {
-    id: 2,
-    author: 'newjeans_offical2',
-    title: '뉴진스 해린',
-    content: '춤 추고 있는 해린.',
-    likeCount: 100,
-    commentCount: 150,
-  },
-  {
-    id: 3,
-    author: 'newjeans_offical3',
-    title: '뉴진스 하니',
-    content: '노래 부르고 있는 하니.',
-    likeCount: 100,
-    commentCount: 150,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -46,21 +10,15 @@ export class PostsController {
   // 1) GET /posts
   // 모든 게시물을 조회하는 API
   @Get()
-  getPosts() : PostModel[]{
-    return posts;
-  }
+  getPosts() {
+    return this.postsService.getAllPosts();
+  } 
 
   // 2) GET /posts/:id
   // 아이디에 해당되는 특정 게시물을 조회하는 API
   @Get(':id')
-  getPost(@Param('id') id: string) : PostModel{
-    const post = posts.find((post) => post.id === +id );
-
-    if(!post){
-      throw new NotFoundException('게시물을 찾을 수 없습니다.');
-    }
-    return post;
-    
+  getPost(@Param('id') id: string) {
+   return this.postsService.getPostById(+id);
   }
 
   // 3) POST /posts
@@ -71,19 +29,9 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ){
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author: author,
-      title: title,
-      content: content,
-      likeCount: 0,
-      commentCount: 0,
-    }
-    posts = [
-      ...posts,
-      post,
-    ]
-    return post;
+   return this.postsService.createPost(
+    author, title, content,
+    );
   }
 
   // 4) PATCH /posts/:id
@@ -95,33 +43,14 @@ export class PostsController {
     @Body('title') title?: string,
     @Body('content') content?: string,
   ){
-    const post = posts.find((post) => post.id === +id );
-    if(!post){
-      throw new NotFoundException('게시물을 찾을 수 없습니다.');
-    }
-    if(author){
-      post.author = author;
-    }
-    if(title){
-      post.title = title;
-    }
-    if(content){
-      post.content = content;
-    }
-    posts = posts.map(prevPost => prevPost.id === +id ? post : prevPost);
-    return post;
+   return this.postsService.updatePost(+id, author, title, content);
   }
 
   // 5) DELETE /posts/:id
   // 아이디에 해당되는 특정 게시물을 삭제하는 API
   @Delete(':id')
   deletePost(@Param('id') id: string){
-    const post = posts.find((post) => post.id === +id );
-    if(!post){
-      throw new NotFoundException('게시물을 찾을 수 없습니다.');
-    }
-    posts = posts.filter((post) => post.id !== +id);
-    return id;
+   return this.postsService.deletePost(+id);
   }
   
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+
+export interface PostModel {
+    id: number;
+    author: string;
+    title: string;
+    content: string;
+    likeCount: number;
+    commentCount: number;
+  }
+  
+  let posts : PostModel[] = [
+    {
+      id: 1,
+      author: 'newjeans_offical',
+      title: '뉴진스 민지',
+      content: '메이크업 고치고 있는 민지.',
+      likeCount: 100,
+      commentCount: 150,
+    },
+    {
+      id: 2,
+      author: 'newjeans_offical2',
+      title: '뉴진스 해린',
+      content: '춤 추고 있는 해린.',
+      likeCount: 100,
+      commentCount: 150,
+    },
+    {
+      id: 3,
+      author: 'newjeans_offical3',
+      title: '뉴진스 하니',
+      content: '노래 부르고 있는 하니.',
+      likeCount: 100,
+      commentCount: 150,
+    },
+  ];
+  
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    getAllPosts() : PostModel[]{
+        return posts;
+    }
+
+    getPostById(id: number) : PostModel{
+        const post = posts.find((post) => post.id === +id );
+
+        if(!post){
+          throw new NotFoundException('게시물을 찾을 수 없습니다.');
+        }
+        return post;
+    }
+
+    createPost(author: string, title: string, content: string,){
+        const post: PostModel = {
+            id: posts[posts.length - 1].id + 1,
+            author: author,
+            title: title,
+            content: content,
+            likeCount: 0,
+            commentCount: 0,
+          }
+          posts = [
+            ...posts,
+            post,
+          ]
+          return post;
+    }
+
+    updatePost(postId: number, author?: string, title?: string, content?: string){
+        const post = posts.find((post) => post.id === postId );
+        if(!post){
+          throw new NotFoundException('게시물을 찾을 수 없습니다.');
+        }
+        if(author){
+          post.author = author;
+        }
+        if(title){
+          post.title = title;
+        }
+        if(content){
+          post.content = content;
+        }
+        posts = posts.map(prevPost => prevPost.id === postId ? post : prevPost);
+        return post;
+    };
+
+    deletePost(postId: number){ const post = posts.find((post) => post.id === postId );
+        if(!post){
+          throw new NotFoundException('게시물을 찾을 수 없습니다.');
+        }
+        posts = posts.filter((post) => post.id !== postId);
+        return postId;
+    }
+}
