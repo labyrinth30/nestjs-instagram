b2734bab5c7ab48fdba35620ed39baf02cab4535
younha00 <ayeon0@office.skhu.ac.kr>
2024-02-13 20:10:09 +0900

    feat: extractTokenFromHeader 메서드

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,6 +11,57 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
   ) {}
+
+  /**
+   * 토큰을 사용하게 되는 방식
+   *
+   * 1) 사용자가 로그인 또는 회원가입을 진행하면
+   *  - accessToken과 refreshToken을 발급한다.
+   *
+   * 2) 로그인 할 때는 Basic 토큰과 함께 요청을 보낸다.
+   *    Basic 토큰은 '이메일:비밀번호'를 base64로 인코딩한 값이다.
+   *    예) {authorization: 'Basic {token}'}
+   *
+   * 3) 아무나 접근할 수 없는 정보를 접근 할 때는
+   *    accessToken을 헤더에 담아서 요청을 보낸다.
+   *    예) {authorization: 'Bearer {accessToken}'}
+   *
+   * 4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을 보낸
+   *    사용자가 누구인지 알 수 있다.
+   *    예를 들어 현재 로그인한 사용자가 작성한 포스트만 가져오려면
+   *    토큰의 sub 값에 입력되어있는 사용자의 포스트만 가져오면 된다.
+   *    특정 사용자의 토큰이 없다면 달느 사용자의 데이터를 접근 못한다.
+   *
+   * 5) 모든 토큰은 만료 기간이 있다. 만료 기간이 지나면 토큰을 다시 발급 받아야 한다.
+   *    그렇지 않으면 jwtService.verify()에서 에러가 발생한다.
+   *    그러니 access 토큰을 새로 발급 받을 수 있도록 /auth/token/access랑
+   *    refresh 토큰을 새로 발급 받을 수 있도록 /auth/token/refresh를 만들어야 한다.
+   *
+   * 6) 토큰이 만료되면 각각의 토큰을 새로 발급 받을 수 있는 엔드포인트에 요청을 해서
+   *    새로운 토큰을 발급받고 새로운 토큰을 사용해서 private route에 접근한다
+   */
+
+  /**
+   * Header로부터 토큰을 받을 때
+   *
+   * {authorization: 'Basic {token}'}
+   * {authorization: 'Bearer {token}'}
+   */
+
+  async extractTokenFromHeader(header: string, isBearer: boolean) {
+    // 'Basic {token}' -> ['Basic', '{token}']
+    // 'Bearer {token}' -> ['Bearer', '{token}']
+    const splitToken = header.split(' ');
+
+    const prefix = isBearer ? 'Bearer' : 'Basic';
+
+    if(splitToken.length !== 2 || splitToken[0] !== prefix){
+      throw new UnauthorizedException('토큰이 올바르지 않습니다.');
+    }
+    const token = splitToken[1];
+    return token;
+  }
+
   /**
    * 우리가 만드려는 기능
    *
