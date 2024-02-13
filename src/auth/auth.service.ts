07fc0dad91534d918af9cdf053e5b0607285e68d
younha00 <ayeon0@office.skhu.ac.kr>
2024-02-13 15:07:22 +0900

    feat: jwt signing 해보기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from '../users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+  ) {}
   /**
    * 우리가 만드려는 기능
    *
@@ -9,6 +15,7 @@ export class AuthService {
    *    - email, nickname. password를 받아서 새로운 유저를 생성하는 기능
    *    - 생성이 완료되면, accessToken과 refreshToken을 발급한다.
    *    회원가입 후 다시 로그인해주세요 <- 이런 쓸데없는 과정을 방지하기 위해서
+   *
    *
    * 2) loginWithEmail
    *    - email, password를 받아서 로그인하는 기능
@@ -28,4 +35,26 @@ export class AuthService {
    *    4. loginWithEmail에서 반환된 데이터를 기반으로 토큰 생성
    *
    */
+  /**
+   * Payload에 들어갈 정보
+   *
+   * 1) email
+   * 2) sub -> id
+   * 3) type: 'access' | 'refresh'
+   *
+   * 사용자 정보 중에 email, id를 가지고 토큰을 발급한다.
+   */
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean){
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      // seconds
+      expiresIn: isRefreshToken ? '3600' : '300',
+    })
+  }
 }
---
+++ src/auth/const/auth.const.ts
@@ -0,0 +1,2 @@
+// 추후 .env 파일로 변경
+export const JWT_SECRET = 'secret123';
