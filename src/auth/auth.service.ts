ad201336f2ba3b6e9b18b8cb3d915990ca8b46fb
younha00 <ayeon0@office.skhu.ac.kr>
2024-02-15 13:11:31 +0900

    feat: Custom Decorator 생성 및 verifyToken에 Exception 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -88,9 +88,13 @@ export class AuthService {
    * 토큰을 검증하는 방법
    */
   verifyToken(token: string) {
-    return this.jwtService.verify(token, {
-      secret: JWT_SECRET,
-    })
+      try{
+        return this.jwtService.verify(token, {
+          secret: JWT_SECRET,
+        })
+      } catch(e){
+          throw new UnauthorizedException('토큰이 만료되었거나 잘못된 토큰입니다.');
+      }
   }
 
   /**
---
+++ src/users/decorator/user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext, InternalServerErrorException } from '@nestjs/common';
+
+// data는 데코레이터를 사용할 때 넣어주는 값이다.
+// context는 guard나 interceptor에서 사용하는 것과 동일하다.
+export const User = createParamDecorator((data, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+    if(!user){
+      throw new InternalServerErrorException('User 데코레이터는 AccessTokenGuard와 함께 사용해야합니다. Request에 user 프로퍼티가 존재하지 않습니다');
+    }
+    return user;
+});
