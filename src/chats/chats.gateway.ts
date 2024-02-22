4ef08e334add36925de2deeb682411d35356ffbe
younha00 <ayeon0@office.skhu.ac.kr>
2024-02-22 19:30:59 +0900

    ✨ Room으로 방을 나누기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,4 +1,5 @@
 import {
+  ConnectedSocket,
   MessageBody,
   OnGatewayConnection,
   SubscribeMessage,
@@ -20,10 +21,25 @@ export class ChatsGateway implements OnGatewayConnection {
     console.log(`on connect called: ${socket.id}`);
   }
   // socket.on('send_message', (message) => { console.log(message); });
+
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    // 방의 chat Id들을 리스트로 받는다.
+    @MessageBody() data: number[],
+    @ConnectedSocket() socket: Socket,
+  ){
+    for(const chatId of data){
+      // socket.join() 실행해야 한다.
+      socket.join(chatId.toString());
+    }
+  }
+
   @SubscribeMessage('send_message')
   sendMessage(
-    @MessageBody() data: string
+    @MessageBody() message: { message: string, chatId: number },
+    @ConnectedSocket() socket: Socket,
   ){
-    this.server.emit('receive_message', 'hello from server');
+    // 선택한 chatID의 방에 있는 사용자만 메시지를 받는다.
+    this.server.in(message.chatId.toString()).emit('receive_message', message.message);
   }
 }
