4edd94eaa61e1533e6260223e77b3947dfc04a9b
younha00 <ayeon0@office.skhu.ac.kr>
2024-02-29 21:33:04 +0900

    ✨ Feat: /health api 추가
    AWS용 /health api 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from './common/decorator/is-public.decorator';
 
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/health')
+  @IsPublic()
+  getHealth(): string {
+    return 'ok';
+  }
 }
